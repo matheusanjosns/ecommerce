a8c1e98cac436e626a02e882fa9acbccc803ea24
matheusanjos <59987414+matheusanjosns@users.noreply.github.com>
2022-03-16 22:01:50 -0300

    fix: implementando tela de login adm

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
     public function __construct($opts = array(), $tpl_dir = "/views/"){
@@ -27,7 +29,7 @@ class page {
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"]=== true) $this->tpl->draw("header");
     }
 
     private function setData($data = array()){
@@ -47,7 +49,7 @@ class page {
 
     public function __destruct(){
         
-        $this->tpl->draw("footer");
+        if ($this->options["footer"]=== true)  $this->tpl->draw("footer");
 
 
     }

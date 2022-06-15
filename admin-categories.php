847238f713e2d382ebac48307bc1d9bf8bdbfaf7
matheusanjos <59987414+matheusanjosns@users.noreply.github.com>
2022-06-15 00:06:43 -0300

    feat inserindo produto em categorias

--- admin-categories.php
+++ admin-categories.php
@@ -4,6 +4,7 @@ use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
+use Hcode\Model\Product;
 
 $app->get("/admin/categories", function(){
 
@@ -70,20 +71,6 @@ $app->get("/admin/categories/:idcategory", function($idcategory){
 	]);
 });
 
-$app->get("/categories/:idcategory", function($idcategory){
-
-	$category = new Category();
-
-	$category->get((int)$idcategory);
-
-	$page = new Page();
-
-	$page->setTpl("category", [
-		'category'=>$category->getValues(),
-		'products'=>[]
-	]);
-
-});
 
 $app->post("/admin/categories/:idcategory", function($idcategory){
 
@@ -98,6 +85,61 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	$category->save();
 
 	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/products", function($idcategory){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-products", [
+		'category'=>$category->getValues(),
+		'productsRelated'=>$category->getProducts(),
+		'productsNotRelated'=>$category->getProducts(false)
+	]);
+
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$category->addProduct($product);
+
+	header("Location: /admin/categories/".$idcategory. "/products");
+	exit;
+});
+
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$category->removeProduct($product);
+
+	header("Location: /admin/categories/".$idcategory. "/products");
 	exit;
 });
 
--- site.php
+++ site.php
@@ -1,6 +1,7 @@
 <?php
 
 use Hcode\Model\Product;
+use Hcode\Model\Category;
 use \Hcode\Page;
 
 $app->get('/', function() {
@@ -12,6 +13,21 @@ $app->get('/', function() {
 	$page->setTpl("index", [
 	 'products'=>Product::checkList($products)
 	]);
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+	]);
+
 });
 
 ?>
